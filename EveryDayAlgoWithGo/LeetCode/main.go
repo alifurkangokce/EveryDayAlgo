8551b69a3aad88f602749f8dad8d97176921fcaf
Ali Furkan GÖKÇE <alifurkangokce@gmail.com>
2022-03-29 15:00:01 +0300

    longestCommonPrefix 0ms

--- EveryDayAlgoWithGo/LeetCode/main.go
+++ EveryDayAlgoWithGo/LeetCode/main.go
@@ -5,5 +5,6 @@ import "fmt"
 func main() {
 	//fmt.Print(TwoSum([]int{3, 2, 4}, 6))
 	// fmt.Print(isPalindrome(121))
-	fmt.Print(romanToInt("MCMXCIV"))
+	//fmt.Print(romanToInt("MCMXCIV"))
+	fmt.Print(longestCommonPrefix([]string{"flower", "flow", "flight"}))
 }
