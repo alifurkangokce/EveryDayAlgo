53a444dbcc346330bdc7bc43a61dff4c2be3b1be
Ali Furkan GÖKÇE <alifurkangokce@gmail.com>
2022-03-30 10:39:31 +0300

    ValidParantheses 0ms

---
+++ EveryDayAlgoWithGo/LeetCode/ValidParantheses.go
@@ -0,0 +1,24 @@
+package main
+
+func isValid(s string) bool {
+	var q []rune
+	for _, c := range s {
+
+		switch c {
+		case '(':
+			q = append(q, ')')
+		case '{':
+			q = append(q, '}')
+		case '[':
+			q = append(q, ']')
+		default:
+			if len(q) == 0 || c != q[len(q)-1] {
+				return false
+			}
+			q = q[:len(q)-1]
+		}
+
+	}
+
+	return len(q) == 0
+}
--- EveryDayAlgoWithGo/LeetCode/main.go
+++ EveryDayAlgoWithGo/LeetCode/main.go
@@ -6,5 +6,6 @@ func main() {
 	//fmt.Print(TwoSum([]int{3, 2, 4}, 6))
 	// fmt.Print(isPalindrome(121))
 	//fmt.Print(romanToInt("MCMXCIV"))
-	fmt.Print(longestCommonPrefix([]string{"flower", "flow", "flight"}))
+	//fmt.Print(longestCommonPrefix([]string{"flower", "flow", "flight"}))
+	fmt.Print(isValid("(([]))"))
 }
