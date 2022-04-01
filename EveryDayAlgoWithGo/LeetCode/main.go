bbb74d8029d552a520730016aa03d480e1208eff
Ali Furkan GÖKÇE <alifurkangokce@gmail.com>
2022-04-01 15:28:51 +0300

    RemoveDuplicates 4 ms

--- EveryDayAlgoWithGo/LeetCode/main.go
+++ EveryDayAlgoWithGo/LeetCode/main.go
@@ -8,5 +8,6 @@ func main() {
 	//fmt.Print(romanToInt("MCMXCIV"))
 	//fmt.Print(longestCommonPrefix([]string{"flower", "flow", "flight"}))
 	//fmt.Print(isValid("(([]))"))
-	fmt.Print(mergeTwoLists(&ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 4}}}, &ListNode{Val: 1, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4}}}))
+	//fmt.Print(mergeTwoLists(&ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 4}}}, &ListNode{Val: 1, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4}}}))
+	fmt.Print(removeDuplicates([]int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}))
 }
