d89c73eaed77eaf389a3563e73f95f1411195e3a
Ali Furkan GÖKÇE <alifurkangokce@gmail.com>
2022-04-03 04:49:50 +0300

    ImplementstrStr 0ms

--- EveryDayAlgoWithGo/LeetCode/main.go
+++ EveryDayAlgoWithGo/LeetCode/main.go
@@ -9,5 +9,6 @@ func main() {
 	//fmt.Print(longestCommonPrefix([]string{"flower", "flow", "flight"}))
 	//fmt.Print(isValid("(([]))"))
 	//fmt.Print(mergeTwoLists(&ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 4}}}, &ListNode{Val: 1, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4}}}))
-	fmt.Print(removeDuplicates([]int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}))
+	//fmt.Print(removeDuplicates([]int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}))
+	fmt.Print(strStr("hello", "ll"))
 }
