3378cec453d6f5bfeab4f354dbe788fcdf9722ab
Ali Furkan GÖKÇE <alifurkangokce@gmail.com>
2022-09-25 15:21:47 +0300

    Invert Binary Tree 2 ms

--- EveryDayAlgoWithGo/LeetCode/main.go
+++ EveryDayAlgoWithGo/LeetCode/main.go
@@ -56,10 +56,11 @@ func main() {
 	//fmt.Print(reverseList(&ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 6, Next: nil}}}))
 	//fmt.Print(containsDuplicate([]int{1, 2, 3, 2}))
 	//fmt.Print(containsNearbyDuplicate([]int{1, 0, 1, 1}, 1))
-	x := MyStack{}
-	x.Push(1)
-	x.Push(2)
-	fmt.Println(x.Top())
-	fmt.Println(x.Pop())
-	fmt.Println(x.Empty())
+	// x := MyStack{}
+	// x.Push(1)
+	// x.Push(2)
+	// fmt.Println(x.Top())
+	// fmt.Println(x.Pop())
+	// fmt.Println(x.Empty())
+	fmt.Print(invertTree(&TreeNode{Val: 4, Left: &TreeNode{Val: 2, Left: &TreeNode{Val: 1}, Right: &TreeNode{Val: 3}}, Right: &TreeNode{Val: 7, Left: &TreeNode{Val: 6}, Right: &TreeNode{Val: 9}}}))
 }
