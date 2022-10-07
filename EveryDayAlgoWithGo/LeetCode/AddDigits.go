e9dcb3e356c726c90e940f03fc01a39555fa7009
Ali Furkan GÖKÇE <alifurkangokce@gmail.com>
2022-10-07 06:35:13 +0300

    Ugly Number 0ms

--- EveryDayAlgoWithGo/LeetCode/AddDigits.go
+++ EveryDayAlgoWithGo/LeetCode/AddDigits.go
@@ -1,8 +1,14 @@
 package main
 
 func addDigits(num int) int {
-	for num%10 != num {
-		num = (num % 10) + (num-(num%10))/10
+	// for num%10 != num {
+	// 	num = (num % 10) + (num-(num%10))/10
+	// }
+	// return num
+
+	// Best Solution
+	if num == 0 {
+		return 0
 	}
-	return num
+	return 1 + (num-1)%9
 }
