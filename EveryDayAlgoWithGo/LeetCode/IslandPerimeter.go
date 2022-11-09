04a044113a62d6cddda8d2ec56d49bdc6faa0553
Ali Furkan Gökçe <alifurkangokce@gmail.com>
2022-11-09 13:27:50 +0300

    Island Perimeter

--- EveryDayAlgoWithGo/LeetCode/IslandPerimeter.go
+++ EveryDayAlgoWithGo/LeetCode/IslandPerimeter.go
@@ -1,15 +1,23 @@
 package main
 
 func islandPerimeter(grid [][]int) int {
-
-	res := 0
+	var perimeter int
 	for i := 0; i < len(grid); i++ {
-		pre := 0
-		for j := 0; j < len(grid[i]); j++ {
-			if grid[i][j] == 1 {
-				res += 4
+		for j := 0; j < len(grid[0]); j++ {
+			if grid[i][j] == 0 {
+				continue
+			}
+			perimeter += 4
+			for _, diff := range [][]int{{0, 1}, {0, -1}, {-1, 0}, {1, 0}} {
+				x, y := j+diff[0], i+diff[1]
+				if y < 0 || y >= len(grid) || x < 0 || x >= len(grid[0]) {
+					continue
+				}
+				if grid[y][x] == 1 {
+					perimeter--
+				}
 			}
 		}
 	}
-	return 0
+	return perimeter
 }
