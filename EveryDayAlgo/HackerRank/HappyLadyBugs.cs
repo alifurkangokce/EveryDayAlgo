9297d6895ed2d52ec380c3ae8c782bf59bb58b7d
Ali Furkan Gökçe <alifurkangokce@gmail.com>
2023-01-07 14:19:28 +0300

    Strange Code

--- EveryDayAlgo/HackerRank/HappyLadyBugs.cs
+++ EveryDayAlgo/HackerRank/HappyLadyBugs.cs
@@ -1,97 +1,97 @@
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
+// using System.CodeDom.Compiler;
+// using System.Collections.Generic;
+// using System.Collections;
+// using System.ComponentModel;
+// using System.Diagnostics.CodeAnalysis;
+// using System.Globalization;
+// using System.IO;
+// using System.Linq;
+// using System.Reflection;
+// using System.Runtime.Serialization;
+// using System.Text.RegularExpressions;
+// using System.Text;
+// using System;
 
-class Result
-{
+// class Result
+// {
 
-    /*
-     * Complete the 'happyLadybugs' function below.
-     *
-     * The function is expected to return a STRING.
-     * The function accepts STRING b as parameter.
-     */
+//     /*
+//      * Complete the 'happyLadybugs' function below.
+//      *
+//      * The function is expected to return a STRING.
+//      * The function accepts STRING b as parameter.
+//      */
 
-    public static string happyLadybugs(string b)
-    {
-        var dic=new Dictionary<char,int>();
-        if (b.Contains('_'))
-        {
-            for (int i = 0; i <= b.Length-1; i++)
-            {
-                if (b[i]!='_')
-                {
-                    if (!dic.ContainsKey(b[i]))
-                    {
-                         dic.Add(b[i],1);
-                    }else{
-                        dic[b[i]]++;
-                    }
+//     public static string happyLadybugs(string b)
+//     {
+//         var dic=new Dictionary<char,int>();
+//         if (b.Contains('_'))
+//         {
+//             for (int i = 0; i <= b.Length-1; i++)
+//             {
+//                 if (b[i]!='_')
+//                 {
+//                     if (!dic.ContainsKey(b[i]))
+//                     {
+//                          dic.Add(b[i],1);
+//                     }else{
+//                         dic[b[i]]++;
+//                     }
                    
-                }
-            }
-            foreach (var item in dic)
-            {
-                if (item.Value<=1)
-                {
-                    return "NO";
-                }
-            }
-        }else{
-            if (b.Length==1)
-            {
-                return "NO";
-            }
-            for (int i = 1; i <= b.Length-1; i++)
-            {
-                if (b[i]!=b[i-1] && i!=b.Length-1)
-                {
-                    if (b[i]!=b[i+1])
-                    {
-                        return "NO";
-                    }
-                }else if(b[i]!=b[i-1] && i==b.Length-1){
-                    return "NO";
-                }
-            }
-        }
-        return "YES";
+//                 }
+//             }
+//             foreach (var item in dic)
+//             {
+//                 if (item.Value<=1)
+//                 {
+//                     return "NO";
+//                 }
+//             }
+//         }else{
+//             if (b.Length==1)
+//             {
+//                 return "NO";
+//             }
+//             for (int i = 1; i <= b.Length-1; i++)
+//             {
+//                 if (b[i]!=b[i-1] && i!=b.Length-1)
+//                 {
+//                     if (b[i]!=b[i+1])
+//                     {
+//                         return "NO";
+//                     }
+//                 }else if(b[i]!=b[i-1] && i==b.Length-1){
+//                     return "NO";
+//                 }
+//             }
+//         }
+//         return "YES";
 
-    }
+//     }
 
-}
+// }
 
-class Solution
-{
-    public static void Main(string[] args)
-    {
-        // TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+// class Solution
+// {
+//     public static void Main(string[] args)
+//     {
+//         // TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-        int g = Convert.ToInt32(Console.ReadLine().Trim());
+//         int g = Convert.ToInt32(Console.ReadLine().Trim());
 
-        for (int gItr = 0; gItr < g; gItr++)
-        {
-            int n = Convert.ToInt32(Console.ReadLine().Trim());
+//         for (int gItr = 0; gItr < g; gItr++)
+//         {
+//             int n = Convert.ToInt32(Console.ReadLine().Trim());
 
-            string b = Console.ReadLine();
+//             string b = Console.ReadLine();
 
-            string result = Result.happyLadybugs(b);
+//             string result = Result.happyLadybugs(b);
 
-            // textWriter.WriteLine(result);
-            Console.WriteLine(result);
-        }
+//             // textWriter.WriteLine(result);
+//             Console.WriteLine(result);
+//         }
 
-        // textWriter.Flush();
-        // textWriter.Close();
-    }
-}
+//         // textWriter.Flush();
+//         // textWriter.Close();
+//     }
+// }
---
+++ EveryDayAlgo/HackerRank/StrangeCode.cs
@@ -0,0 +1,48 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static long strangeCounter(long t)
+    {
+        long startTime = 1;
+        long startValue = 3;
+        while(t>=(startTime*2)+2){
+            startTime=(startTime*2)+2;
+            startValue*=2;
+        }
+
+        long offset = (t - startTime);
+        
+        return (startValue - offset);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        long t = Convert.ToInt64(Console.ReadLine().Trim());
+
+        long result = Result.strangeCounter(t);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
