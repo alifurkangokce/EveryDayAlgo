a1a8df87d0fe10b14c1dfa5b5768e1fe5f7710b6
Ali Furkan Gökçe <alifurkangokce@gmail.com>
2023-01-11 15:30:44 +0300

    Big Sorting

--- EveryDayAlgo/HackerRank/StrangeCode.cs
+++ EveryDayAlgo/HackerRank/StrangeCode.cs
@@ -1,48 +1,48 @@
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
+// using System.CodeDom.Compiler;
+// using System.Collections.Generic;
+// using System.Collections;
+// using System.ComponentModel;
+// using System.Diagnostics.CodeAnalysis;
+// using System.Globalization;
+// using System.IO;
+// using System.Linq;
+// using System.Reflection;
+// using System.Runtime.Serialization;
+// using System.Text.RegularExpressions;
+// using System.Text;
+// using System;
 
-class Result
-{
-    public static long strangeCounter(long t)
-    {
-        long startTime = 1;
-        long startValue = 3;
-        while(t>=(startTime*2)+2){
-            startTime=(startTime*2)+2;
-            startValue*=2;
-        }
+// class Result
+// {
+//     public static long strangeCounter(long t)
+//     {
+//         long startTime = 1;
+//         long startValue = 3;
+//         while(t>=(startTime*2)+2){
+//             startTime=(startTime*2)+2;
+//             startValue*=2;
+//         }
 
-        long offset = (t - startTime);
+//         long offset = (t - startTime);
         
-        return (startValue - offset);
-    }
+//         return (startValue - offset);
+//     }
 
-}
+// }
 
-class Solution
-{
-    public static void Main(string[] args)
-    {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+// class Solution
+// {
+//     public static void Main(string[] args)
+//     {
+//         TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-        long t = Convert.ToInt64(Console.ReadLine().Trim());
+//         long t = Convert.ToInt64(Console.ReadLine().Trim());
 
-        long result = Result.strangeCounter(t);
+//         long result = Result.strangeCounter(t);
 
-        textWriter.WriteLine(result);
+//         textWriter.WriteLine(result);
 
-        textWriter.Flush();
-        textWriter.Close();
-    }
-}
+//         textWriter.Flush();
+//         textWriter.Close();
+//     }
+// }
