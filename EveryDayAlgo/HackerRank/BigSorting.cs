46848e130f6e536a418146a4d204725f7ee15b1c
Ali Furkan Gökçe <alifurkangokce@gmail.com>
2023-01-12 14:46:09 +0300

    Super Reduced String

--- EveryDayAlgo/HackerRank/BigSorting.cs
+++ EveryDayAlgo/HackerRank/BigSorting.cs
@@ -1,56 +1,56 @@
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
+// using System.CodeDom.Compiler;
+// using System.Collections.Generic;
+// using System.Collections;
+// using System.ComponentModel;
+// using System.Diagnostics.CodeAnalysis;
+// using System.Globalization;
+// using System.IO;
+// using System.Linq;
+// using System.Reflection;
+// using System.Runtime.Serialization;
+// using System.Text.RegularExpressions;
+// using System.Text;
+// using System;
 
-class Result
-{
+// class Result
+// {
 
-    /*
-     * Complete the 'bigSorting' function below.
-     *
-     * The function is expected to return a STRING_ARRAY.
-     * The function accepts STRING_ARRAY unsorted as parameter.
-     */
+//     /*
+//      * Complete the 'bigSorting' function below.
+//      *
+//      * The function is expected to return a STRING_ARRAY.
+//      * The function accepts STRING_ARRAY unsorted as parameter.
+//      */
 
-    public static List<string> bigSorting(List<string> unsorted)
-    {
+//     public static List<string> bigSorting(List<string> unsorted)
+//     {
         
-        return unsorted.OrderBy(x=>x.Length).ThenBy(x=>x).ToList();
-    }
+//         return unsorted.OrderBy(x=>x.Length).ThenBy(x=>x).ToList();
+//     }
 
-}
+// }
 
-class Solution
-{
-    public static void Main(string[] args)
-    {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+// class Solution
+// {
+//     public static void Main(string[] args)
+//     {
+//         TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-        int n = Convert.ToInt32(Console.ReadLine().Trim());
+//         int n = Convert.ToInt32(Console.ReadLine().Trim());
 
-        List<string> unsorted = new List<string>();
+//         List<string> unsorted = new List<string>();
 
-        for (int i = 0; i < n; i++)
-        {
-            string unsortedItem = Console.ReadLine();
-            unsorted.Add(unsortedItem);
-        }
+//         for (int i = 0; i < n; i++)
+//         {
+//             string unsortedItem = Console.ReadLine();
+//             unsorted.Add(unsortedItem);
+//         }
 
-        List<string> result = Result.bigSorting(unsorted);
+//         List<string> result = Result.bigSorting(unsorted);
 
-        textWriter.WriteLine(String.Join("\n", result));
+//         textWriter.WriteLine(String.Join("\n", result));
 
-        textWriter.Flush();
-        textWriter.Close();
-    }
-}
+//         textWriter.Flush();
+//         textWriter.Close();
+//     }
+// }
