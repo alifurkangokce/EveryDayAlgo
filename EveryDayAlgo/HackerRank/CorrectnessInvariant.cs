d6fdd8760497b86a4c50a0fc12bbe6f854294cb4
Ali Furkan Gökçe <alifurkangokce@gmail.com>
2023-02-21 18:06:05 +0300

    Correctness Invariant

---
+++ EveryDayAlgo/HackerRank/CorrectnessInvariant.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+class Solution { 
+    public static void insertionSort (int[] A) { 
+        Console.WriteLine(string.Join(" ", A.OrderBy(x=>x))); 
+    }
+
+    static void Main(string[] args) { 
+        Console.ReadLine(); 
+        int [] _ar = (from s in Console.ReadLine().Split() select Convert.ToInt32(s)).ToArray();
+        insertionSort(_ar); 
+    }
+}
+
