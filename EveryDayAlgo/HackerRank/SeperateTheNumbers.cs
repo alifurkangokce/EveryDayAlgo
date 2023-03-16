af235502b5aeda37cd7bce01f8c9cb6b8b4d6368
Ali Furkan Gökçe <alifurkangokce@gmail.com>
2023-03-16 17:41:55 +0300

    Seperate The Numbers

---
+++ EveryDayAlgo/HackerRank/SeperateTheNumbers.cs
@@ -0,0 +1,66 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.Numerics;
+
+class Result
+{
+
+    /*
+     * Complete the 'separateNumbers' function below.
+     *
+     * The function accepts STRING s as parameter.
+     */
+
+        public static void separateNumbers(string s)
+        {
+            string newS = "";
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                newS = "";
+                BigInteger returnInt = BigInteger.Parse(new String(s.Skip(0).Take(i + 1).ToArray()));
+                BigInteger firstInt = BigInteger.Parse(new String(s.Skip(0).Take(i + 1).ToArray()));
+                while (newS.Length < s.Length)
+                {
+                    BigInteger nextInt = firstInt++;
+                    newS += nextInt.ToString();
+                }
+                if (s == newS)
+                {
+                    Console.WriteLine("YES " + returnInt.ToString());
+                    break;
+                }
+            }
+            if (s != newS)
+            {
+                Console.WriteLine("NO");
+            }
+        }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        // int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        // for (int qItr = 0; qItr < q; qItr++)
+        // {
+        //     string s = Console.ReadLine();
+
+        //     Result.separateNumbers(s);
+        // }
+        Result.separateNumbers("99910001001");
+    }
+}
--- EveryDayAlgo/HackerRank/WeightedUniformString.cs
+++ EveryDayAlgo/HackerRank/WeightedUniformString.cs
@@ -1,78 +1,78 @@
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
+// using System.CodeDom.Compiler;
+// using System.Collections.Generic;
+// using System.Collections;
+// using System.ComponentModel;
+// using System.Diagnostics.CodeAnalysis;
+// using System.Globalization;
+// using System.IO;
+// using System.Linq;
+// using System.Reflection;
+// using System.Runtime.Serialization;
+// using System.Text.RegularExpressions;
+// using System.Text;
+// using System;
 
-class Result
-{
+// class Result
+// {
 
-    /*
-     * Complete the 'weightedUniformStrings' function below.
-     *
-     * The function is expected to return a STRING_ARRAY.
-     * The function accepts following parameters:
-     *  1. STRING s
-     *  2. INTEGER_ARRAY queries
-     */
+//     /*
+//      * Complete the 'weightedUniformStrings' function below.
+//      *
+//      * The function is expected to return a STRING_ARRAY.
+//      * The function accepts following parameters:
+//      *  1. STRING s
+//      *  2. INTEGER_ARRAY queries
+//      */
 
-    public static List<string> weightedUniformStrings(string s, List<int> queries)
-    {
-        string alphabet = "abcdefghijklmnopqrstuvwxyz";
-        int weight = 0;
-        List<int> weights = new List<int>();
-        char last = s[0];
-        for(int i = 0; i < s.Length; i++){            
-            char cur = s[i];
-            if(i == 0 || cur != last){
-                weight = (alphabet.IndexOf(cur) + 1);
-            } else {
-                weight += (alphabet.IndexOf(cur) + 1);                
-            }             
-            weights.Add(weight);
-            last = cur;
-        }        
-        List<string> result = new List<string>();
-        for(int i = 0; i < queries.Count; i++){
-            if(weights.Contains(queries[i])) result.Add("Yes");
-            else result.Add("No");
-        }        
-        return result;
-    }
+//     public static List<string> weightedUniformStrings(string s, List<int> queries)
+//     {
+//         string alphabet = "abcdefghijklmnopqrstuvwxyz";
+//         int weight = 0;
+//         List<int> weights = new List<int>();
+//         char last = s[0];
+//         for(int i = 0; i < s.Length; i++){            
+//             char cur = s[i];
+//             if(i == 0 || cur != last){
+//                 weight = (alphabet.IndexOf(cur) + 1);
+//             } else {
+//                 weight += (alphabet.IndexOf(cur) + 1);                
+//             }             
+//             weights.Add(weight);
+//             last = cur;
+//         }        
+//         List<string> result = new List<string>();
+//         for(int i = 0; i < queries.Count; i++){
+//             if(weights.Contains(queries[i])) result.Add("Yes");
+//             else result.Add("No");
+//         }        
+//         return result;
+//     }
 
-}
+// }
 
-class Solution
-{
-    public static void Main(string[] args)
-    {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+// class Solution
+// {
+//     public static void Main(string[] args)
+//     {
+//         TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-        string s = Console.ReadLine();
+//         string s = Console.ReadLine();
 
-        int queriesCount = Convert.ToInt32(Console.ReadLine().Trim());
+//         int queriesCount = Convert.ToInt32(Console.ReadLine().Trim());
 
-        List<int> queries = new List<int>();
+//         List<int> queries = new List<int>();
 
-        for (int i = 0; i < queriesCount; i++)
-        {
-            int queriesItem = Convert.ToInt32(Console.ReadLine().Trim());
-            queries.Add(queriesItem);
-        }
+//         for (int i = 0; i < queriesCount; i++)
+//         {
+//             int queriesItem = Convert.ToInt32(Console.ReadLine().Trim());
+//             queries.Add(queriesItem);
+//         }
 
-        List<string> result = Result.weightedUniformStrings(s, queries);
+//         List<string> result = Result.weightedUniformStrings(s, queries);
 
-        textWriter.WriteLine(String.Join("\n", result));
+//         textWriter.WriteLine(String.Join("\n", result));
 
-        textWriter.Flush();
-        textWriter.Close();
-    }
-}
+//         textWriter.Flush();
+//         textWriter.Close();
+//     }
+// }
