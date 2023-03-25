5d3d5d37c6118828dc106106079946676bd72d63
Ali Furkan Gökçe <alifurkangokce@gmail.com>
2023-03-25 14:11:00 +0300

    Rotate String

--- EveryDayAlgo/LeetCode/RotateString.go
+++ EveryDayAlgo/LeetCode/RotateString.go
@@ -1,11 +1,15 @@
 package main
 
+import "strings"
+
 func rotateString(s string, goal string) bool {
-	for i := 0; i < len(s)-1; i++ {
-		if s == goal {
-			return true
-		}
-		s = string(s[len(s)-1]) + string(s[0:len(s)-1])
-	}
-	return false
+	// for i := 0; i < len(s)-1; i++ {
+	// 	if s == goal {
+	// 		return true
+	// 	}
+	// 	s = string(s[len(s)-1]) + string(s[0:len(s)-1])
+	// }
+	// return false
+
+	return len(s) == len(goal) && strings.Contains(s+s, goal)
 }
