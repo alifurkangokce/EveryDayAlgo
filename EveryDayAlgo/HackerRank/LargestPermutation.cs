0ebc61d4e42c58e2d7398213600a9346e84ae045
Ali Furkan Gökçe <alifurkangokce@gmail.com>
2023-10-10 17:50:32 +0300

    Largest Permutation

--- EveryDayAlgo/HackerRank/LargestPermutation.cs
+++ EveryDayAlgo/HackerRank/LargestPermutation.cs
@@ -1,87 +1,76 @@
-// using System.CodeDom.Compiler;
-// using System.Collections.Generic;
-// using System.Collections;
-// using System.ComponentModel;
-// using System.Diagnostics.CodeAnalysis;
-// using System.Globalization;
-// using System.IO;
-// using System.Linq;
-// using System.Reflection;
-// using System.Runtime.Serialization;
-// using System.Text.RegularExpressions;
-// using System.Text;
-// using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-// class Result
-// {
+class Result
+{
 
-//     /*
-//      * Complete the 'largestPermutation' function below.
-//      *
-//      * The function is expected to return an INTEGER_ARRAY.
-//      * The function accepts following parameters:
-//      *  1. INTEGER k
-//      *  2. INTEGER_ARRAY arr
-//      */
+    /*
+     * Complete the 'largestPermutation' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY arr
+     */
 
-//     public static List<int> largestPermutation(int k, List<int> arr)
-//     {
-//         int n = arr.Count;
-//         Dictionary<int, int> index = new Dictionary<int, int>();
+    public static List<int> largestPermutation(int k, List<int> arr)
+    {
+        var cnt = arr.Count();
+        int i = 0;
+        while (k>0 && i<cnt)
+        {
 
-        
-//         for (int i = 0; i < n; i++)
-//         {
-//             index[arr[i]] = i;
-//         }
 
-//         int maxValue = n;
+            if (arr[i]!=cnt-i)
+            {
+                var max = cnt - i;
+                var maxIndex=arr.IndexOf(max);
+                var arr0 = arr[i];
+                arr[i] = max;
+                arr[maxIndex] = arr0;
+                k--;
+            }
 
-//         for (int i = 0; i < n && k > 0; i++)
-//         {
-//             if (arr[i] != maxValue)
-//             {
-                
-//                 int temp = arr[i];
-//                 arr[i] = maxValue;
-//                 arr[index[maxValue]] = temp;
+            i++;
+            
+        }
+        return arr;
 
-               
-//                 index[temp] = index[maxValue];
-//                 index[maxValue] = i;
+    }
 
-//                 k--;
-//             }
+}
 
-//             maxValue--;
-//         }
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-//         return arr;
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
 
-//     }
+        int n = Convert.ToInt32(firstMultipleInput[0]);
 
-// }
+        int k = Convert.ToInt32(firstMultipleInput[1]);
 
-// class Solution
-// {
-//     public static void Main(string[] args)
-//     {
-//         //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
 
-//         string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+        List<int> result = Result.largestPermutation(k, arr);
+        Console.WriteLine(result);
 
-//         int n = Convert.ToInt32(firstMultipleInput[0]);
+        //textWriter.WriteLine(String.Join(" ", result));
 
-//         int k = Convert.ToInt32(firstMultipleInput[1]);
-
-//         List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
-
-//         List<int> result = Result.largestPermutation(k, arr);
-//         Console.WriteLine(result);
-
-//         //textWriter.WriteLine(String.Join(" ", result));
-
-//         //textWriter.Flush();
-//         //textWriter.Close();
-//     }
-// }
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
