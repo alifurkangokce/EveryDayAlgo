d9be1fc437ab489f1cdeedcc45d5762c2ae3d98c
Ali Furkan Gökçe <alifurkangokce@gmail.com>
2023-10-18 16:42:13 +0300

    Maximizing XOR

---
+++ EveryDayAlgo/HackerRank/MaximizingXor.cs
@@ -0,0 +1,64 @@
+// using System.CodeDom.Compiler;
+// using System.Collections.Generic;
+// using System.Collections;
+// using System.ComponentModel;
+// using System.Diagnostics.CodeAnalysis;
+// using System.Globalization;
+// using System.IO;
+// using System.Linq;
+// using System.Reflection;
+// using System.Runtime.Serialization;
+// using System.Text.RegularExpressions;
+// using System.Text;
+// using System;
+
+// class Result
+// {
+
+//     /*
+//      * Complete the 'maximizingXor' function below.
+//      *
+//      * The function is expected to return an INTEGER.
+//      * The function accepts following parameters:
+//      *  1. INTEGER l
+//      *  2. INTEGER r
+//      */
+
+//     public static int maximizingXor(int l, int r)
+//     {
+//         int max = 0;
+//         for (int i = l; i <= r; i++)
+//         {
+//             for (int j = l; j <= r; j++)
+//             {
+//                 if ((i ^ j) > max)
+//                 {
+//                     max = (i ^ j);
+//                 }
+//             }
+//         }
+
+//         return max;
+//     }
+
+// }
+
+// class Solution
+// {
+//     public static void Main(string[] args)
+//     {
+//         //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+//         int l = Convert.ToInt32(Console.ReadLine().Trim());
+
+//         int r = Convert.ToInt32(Console.ReadLine().Trim());
+
+//         int result = Result.maximizingXor(l, r);
+//         Console.WriteLine(result);
+
+//         //textWriter.WriteLine(result);
+
+//         //textWriter.Flush();
+//         //textWriter.Close();
+//     }
+// }
